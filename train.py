8678960017c50157b6b4efbf702fa94c612887c3
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2020-12-15 19:16:03 +0900

    Update train.py

--- train.py
+++ train.py
@@ -55,6 +55,9 @@ def read_label(file, batch_size):
     anchor_count = 5
     responsibleGrid = np.zeros([FLAGS.batch_size, FLAGS.output_size, FLAGS.output_size, 5, 25])
 
+    detector_mask = np.zeros((FLAGS.batch_size, FLAGS.output_size, FLAGS.output_size, anchor_count, 1))
+    matching_true_boxes = np.zeros((FLAGS.batch_size, FLAGS.output_size, FLAGS.output_size, anchor_count, 5))
+
     full_target_grid = []
     for b in range(batch_size):
         f = open(tf.compat.as_bytes(file[b].numpy()), 'r')
@@ -83,8 +86,8 @@ def read_label(file, batch_size):
             y = y * (1/height)
             h = h * (1/height)
 
-            i, j = int(FLAGS.output_size * y), int(FLAGS.output_size * x)
-            x_cell, y_cell = FLAGS.output_size * x - j, FLAGS.output_size * y - i
+            i, j = int(FLAGS.output_size * x), int(FLAGS.output_size * y)
+            x_cell, y_cell = FLAGS.output_size * x - i, FLAGS.output_size * y - j
             width_cell = w * FLAGS.output_size
             height_cell = h * FLAGS.output_size
 
@@ -104,6 +107,10 @@ def read_label(file, batch_size):
                         best_box = iou
                         best_anchor = i
                 if best_box > 0:
+                    #detector_mask[b, responsibleGridX, responsibleGridY, best_anchor] = 1
+                    #yolo_box = np.array([x_cell, y_cell, width_cell, height_cell, ])
+                    # 이 부분 고쳐야한다.
+
                     responsibleGrid[b][responsibleGridX][responsibleGridY][best_anchor][classes] = 1.    # class
                     responsibleGrid[b][responsibleGridX][responsibleGridY][best_anchor][21:25] = boxData    # box
                     responsibleGrid[b][responsibleGridX][responsibleGridY][best_anchor][FLAGS.num_classes] = 1. # confidence
@@ -130,20 +137,20 @@ def grid_offset(grid_H, grid_W):
 
 def IOU(predict_box, label_box):
 
-    box1_x1 = predict_box[..., 0:1] - predict_box[..., 2:3] / 2
-    box1_y1 = predict_box[..., 1:2] - predict_box[..., 3:4] / 2
-    box1_x2 = predict_box[..., 0:1] + predict_box[..., 2:3] / 2
-    box1_y2 = predict_box[..., 1:2] + predict_box[..., 3:4] / 2
+    box1_x1 = predict_box[..., 0:1] - predict_box[..., 2:3] / 2 # xmin1
+    box1_y1 = predict_box[..., 1:2] - predict_box[..., 3:4] / 2 # ymin1
+    box1_x2 = predict_box[..., 0:1] + predict_box[..., 2:3] / 2 # xmax1
+    box1_y2 = predict_box[..., 1:2] + predict_box[..., 3:4] / 2 # ymax1
 
-    box2_x1 = label_box[..., 0:1] - label_box[..., 2:3] / 2
-    box2_y1 = label_box[..., 1:2] - label_box[..., 3:4] / 2
-    box2_x2 = label_box[..., 0:1] + label_box[..., 2:3] / 2
-    box2_y2 = label_box[..., 1:2] + label_box[..., 3:4] / 2
+    box2_x1 = label_box[..., 0:1] - label_box[..., 2:3] / 2 # xmin2
+    box2_y1 = label_box[..., 1:2] - label_box[..., 3:4] / 2 # ymin2
+    box2_x2 = label_box[..., 0:1] + label_box[..., 2:3] / 2 # xmax2
+    box2_y2 = label_box[..., 1:2] + label_box[..., 3:4] / 2 # ymax2
 
     intersect_x1 = tf.maximum(box1_x1, box2_x1)
     intersect_y1 = tf.maximum(box1_y1, box2_y1)
-    intersect_x2 = tf.maximum(box1_x2, box2_x2)
-    intersect_y2 = tf.maximum(box1_y2, box2_y2)
+    intersect_x2 = tf.minimum(box1_x2, box2_x2)
+    intersect_y2 = tf.minimum(box1_y2, box2_y2)
     intersect_x1 = intersect_x1.numpy()
     intersect_y1 = intersect_y1.numpy()
     intersect_x2 = intersect_x2.numpy()
@@ -171,8 +178,8 @@ def cal_loss(model, images, labels):
         pred = tf.reshape(logits,
                           [FLAGS.batch_size, FLAGS.output_size, FLAGS.output_size, 5, 4+1+FLAGS.num_classes])
         I_obj = labels[:, :, :, :, 20]
-        nI_obj = tf.keras.backend.sum(tf.cast(I_obj > 0.0, tf.float32))
         I_obj = tf.expand_dims(I_obj, -1)
+        nI_obj = tf.keras.backend.sum(tf.cast(I_obj > 0.0, tf.float32))
         predict_xy = tf.nn.sigmoid(pred[:, :, :, :, 0:2])
         predict_xy = predict_xy + offsets
         predict_wh = tf.exp(pred[:, :, :, :, 2:4]) * ANCHORS
@@ -182,26 +189,29 @@ def cal_loss(model, images, labels):
 
         ####################################################################################
         # 박스 좌표 loss
-        xy_loss = 1 * tf.keras.backend.sum(I_obj * tf.square(labels[:, :, :, :, 21:23] - predict_xy)) / (nI_obj + 1e-7)
-        wh_loss = 1 * tf.keras.backend.sum(I_obj * tf.square(
-            tf.sqrt(labels[:, :, :, :, 23:25]) - tf.sqrt(predict_wh))) / (nI_obj + 1e-7)
+        xy_loss = 1 * tf.reduce_sum(I_obj * tf.square(labels[:, :, :, :, 21:23] - predict_xy), [1,2,3,4]) / (nI_obj + 1e-6)
+        wh_loss = 1 * tf.reduce_sum(I_obj * tf.square(
+            tf.sqrt(labels[:, :, :, :, 23:25]) - tf.sqrt(predict_wh)), [1,2,3,4]) / (nI_obj + 1e-6)
         coord_loss = xy_loss + wh_loss  # 논문의 첫 번째 수식
+        #print(coord_loss)
         ####################################################################################
 
         ####################################################################################
         # obect loss (오브젝트가 있을 때의 loss)
         iou = IOU(predict_box, labels[:, :, :, :, 21:25])
-        best_box = tf.keras.backend.max(iou, 3)
-        object_loss = 5 * tf.keras.backend.sum(I_obj * tf.square(iou-object_conf)) / (nI_obj + 1e-7)
+        best_box = tf.keras.backend.max(iou, 4)
+        best_box = tf.expand_dims(best_box, -1)
+        object_loss = 5 * tf.reduce_sum(I_obj * tf.square(iou-object_conf), [1,2,3,4]) / (nI_obj + 1e-6)
+        #print(object_loss)
         ####################################################################################
 
         ####################################################################################
         # no object loss (오브젝트가 없을 때의 loss)
-        no_I_obj = tf.cast(best_box < 0.6, tf.float32)
-        no_I_obj = tf.expand_dims(no_I_obj, -1)
+        no_I_obj = tf.cast(best_box < 0.6, tf.keras.backend.dtype(best_box))
         no_I_obj = no_I_obj * (1 - I_obj)
-        no_I_obj = tf.keras.backend.sum(tf.cast(no_I_obj > 0.0, tf.float32))
-        no_object_loss = 1 * tf.keras.backend.sum(no_I_obj * tf.square(-object_conf)) / (nI_obj + 1e-7)
+        nb_no_I_obj = tf.keras.backend.sum(tf.cast(no_I_obj > 0.0, tf.float32))
+        no_object_loss = 1 * tf.reduce_sum(no_I_obj * tf.square(-object_conf), [1,2,3,4]) / (nb_no_I_obj + 1e-6)
+        #print(no_object_loss)   # 너무큰대!?!?!?
         ####################################################################################
 
         ####################################################################################
@@ -212,10 +222,11 @@ def cal_loss(model, images, labels):
                                                               pred_class,
                                                               from_logits=True)
         class_loss = tf.expand_dims(class_loss, -1) * I_obj
-        class_loss = 1 * tf.keras.backend.sum(class_loss) / (nI_obj + 1e-7)
+        class_loss = 1 * tf.reduce_sum(class_loss, [1,2,3,4]) / (nI_obj + 1e-6)
+        #print(class_loss)
         ####################################################################################
 
-        total_loss = coord_loss + object_loss + no_object_loss + class_loss
+        total_loss = tf.reduce_mean(coord_loss + object_loss + no_object_loss + class_loss)
 
     grads = tape.gradient(total_loss, model.trainable_variables)
     optim.apply_gradients(zip(grads, model.trainable_variables))
@@ -269,7 +280,7 @@ def main():
 
                 if count % 10 == 0:
                     print("Epoch: {} [{}/{}] loss = {}".format(epoch, step + 1, batch_idx, loss))
-
+                # 내잃은 여기 그림그리는곳부터!!!
                 count += 1
 
 if __name__ == "__main__":
